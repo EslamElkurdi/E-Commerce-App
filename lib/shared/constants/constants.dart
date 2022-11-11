56dc551b168197c6d3776b5310eec322410403d8
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 15:23:58 +0200

    IslamGamal add constant colors

--- lib/shared/constants/constants.dart
+++ lib/shared/constants/constants.dart
@@ -10,3 +10,9 @@ const InputDecortion = InputDecoration(
   filled: true,
   contentPadding: EdgeInsets.all(8),
 );
+
+
+// colors
+const BTNpink = Color.fromARGB(255, 241, 39, 100);
+const BTNgreen = Color.fromARGB(255, 73, 179, 105);
+const appbarGreen = Color.fromARGB(255, 76, 141, 95);
