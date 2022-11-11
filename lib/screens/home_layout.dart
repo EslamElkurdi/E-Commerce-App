7ceaf46ec5ec7de52129f1b16a5727f774d97e32
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 17:04:14 +0200

    IslamGamal add AppBar into Home screen

--- lib/screens/home_layout.dart
+++ lib/screens/home_layout.dart
@@ -5,8 +5,39 @@ class HomeLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 76, 141, 95),
+        title: const Text("Home"),
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: const BoxDecoration(
+                        color: Color.fromARGB(211, 164, 255, 193),
+                        shape: BoxShape.circle),
+                    child: const Text(
+                      "8",
+                      style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_shopping_cart)
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 12),
+                child: Text("\$ 128"),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
