1e7ab81de5bf1deb606d4cfeee81deb005c761cc
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 16:39:42 +0200

    IslamGamal create a Navigator for sign in button

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../shared/colors/colors.dart';
@@ -8,42 +9,79 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children:   [
-            const SizedBox(
-              height: 64.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              obscureText: false,
-              decoration: InputDecortion.copyWith(hintText: 'Please Enter Email',)
-          ),
-            const SizedBox(
-              height: 33.0,
-            ),
-            TextField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecortion.copyWith(hintText: 'Please Enter Password'),
-        ),
-            const SizedBox(
-              height: 33.0,
-            ),
-            ElevatedButton(
-              onPressed: (){},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(BTNgreen),
-                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:   [
+              const SizedBox(
+                height: 64.0,
               ),
-              child: Text("sign in", style: TextStyle(fontSize: 19),),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: InputDecortion.copyWith(hintText: 'Please Enter Email',)
             ),
+              const SizedBox(
+                height: 33.0,
+              ),
+              TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: InputDecortion.copyWith(hintText: 'Please Enter Password'),
+          ),
+              const SizedBox(
+                height: 33.0,
+              ),
+              ElevatedButton(
+                onPressed: (){},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+                ),
+                child: const Text("sign in", style: TextStyle(fontSize: 19),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    const Text(
+                        'Don\'t have an account?',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0
 
-    ]
-    ),
-      ));
+                      ),
+                    ),
+                  TextButton(
+                      onPressed: ()
+                      {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)
+                                {
+                                  return const RegisterScreen();
+                                }
+                            )
+                        );
+                      },
+                      child: const Text(
+                        'sign up',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0
+
+                      ),
+                      )
+                  ),
+                ],
+              )
+      ]
+      ),
+        )),
+    );
   }
 }
