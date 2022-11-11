673906faa97959b44d370dd09477cd0766c8d133
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 15:28:52 +0200

    IslamGamal create color file and add colors in it

--- lib/shared/constants/constants.dart
+++ lib/shared/constants/constants.dart
@@ -12,7 +12,3 @@ const InputDecortion = InputDecoration(
 );
 
 
-// colors
-const BTNpink = Color.fromARGB(255, 241, 39, 100);
-const BTNgreen = Color.fromARGB(255, 73, 179, 105);
-const appbarGreen = Color.fromARGB(255, 76, 141, 95);
