ea27f8f263d365b0f9d337d48fa5f8c0190ba8fd
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 14:23:12 +0200

    IslamGamal Create Login Screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 64.0,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
