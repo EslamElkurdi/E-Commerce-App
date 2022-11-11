bd4abf0d3a375bee1ab60338ddb66a9a92158554
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-11 22:25:17 +0200

    IslamGamal add GridView into body in HomeLayout

--- lib/screens/home_layout.dart
+++ lib/screens/home_layout.dart
@@ -89,6 +89,48 @@ class HomeLayout extends StatelessWidget {
           )
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 22),
+        child: GridView.builder(
+            gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 33),
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {},
+                child: GridTile(
+                  footer: GridTileBar(
+// backgroundColor: Color.fromARGB(66, 73, 127, 110),
+                    trailing: IconButton(
+                        color: Color.fromARGB(255, 62, 94, 70),
+                        onPressed: () {},
+                        icon: Icon(Icons.add)),
+
+                    leading: Text("\$12.99"),
+
+                    title: Text(
+                      "",
+                    ),
+                  ),
+                  child: Stack(
+                      children: [
+                     Positioned(
+                      top: -3,
+                      bottom: -9,
+                      right: 0,
+                      left: 0,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(55),
+                          child: Image.asset("assets/images/secondcat.jpg")),
+                    ),
+                  ]),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
